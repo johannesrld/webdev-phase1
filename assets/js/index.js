b5e488d5a278bb4b913a45c9cb24488db46b1fba
johannesrld <71744831+johannesrld@users.noreply.github.com>
2023-11-28 07:16:45 +0800

    Revert 1 commits
    4c36f2d 'chore(js): small update to await/formatting'

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,9 +38,11 @@ submitButton.addEventListener('click', async (event) => {
     submitButton.disabled = true;
     try {
         const request = fetch(requestUrl, { headers: { "X-Api-Key": apiKey }, method: "GET" });
+
         outputContent.textContent = "Calculating value..."; // technically we're not but shhhhhh!!!
 
-        const data = await request.json();
+        const response = await request;
+        const data = await response.json();
 
         outputContent.textContent = `${formatCurrency(fromCurrency, data.old_amount)} is worth ${formatCurrency(toCurrency, data.new_amount)}`;
     }
