ba1698d283ee03c2bbf37e688c4065244c2b1216
johannesrld <71744831+johannesrld@users.noreply.github.com>
2023-11-29 13:29:02 +0800

    feat(html): format js and fix w3c violations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,14 @@
-import currencyCodes from "./currencyCodes.js";
+"use strict";
+
+const currencyCodes = {
+  AUD: "Australian Dollars",
+  CHF: "Swiss Francs",
+  CNY: "Chinese Yuan",
+  EUR: "Euro",
+  PLN: "Polish Złoty",
+  TRY: "Turkish Liras",
+  USD: "United States Dollars"
+};
 
 const apiKey = "qdxvVhmu2hXWag99cJvt4A==jRYsB1KnGXNzZdIB";
 const baseApi = "https://api.api-ninjas.com/v1/convertcurrency";
@@ -13,39 +23,39 @@ const outputContent = document.createElement("p");
 output.append(outputContent);
 
 function formatCurrency(isoCode, value) {
-    return `${(new Intl.NumberFormat(undefined, {
-        currency: isoCode,
-        currencyDisplay: "narrowSymbol",
-        localeMatcher: "lookup",
-        style: "currency",
-    })).format(value)} ${currencyCodes[isoCode]}`; // This would return something like "$1.20 Australian Dollars", "€0.88 Euro"
+  return `${(new Intl.NumberFormat(undefined, {
+    currency: isoCode,
+    currencyDisplay: "narrowSymbol",
+    localeMatcher: "lookup",
+    style: "currency",
+  })).format(value)} ${currencyCodes[isoCode]}`; // This would return something like "$1.20 Australian Dollars", "€0.88 Euro"
 }
 
 for (const [currencyName, isoCode] of Object.entries(currencyCodes)) {
-    [fromCurrencyDropDown, toCurrencyDropDown].forEach(element => {
-        element.add(new Option(isoCode, currencyName));
-    });
+  [fromCurrencyDropDown, toCurrencyDropDown].forEach(element => {
+    element.add(new Option(isoCode, currencyName));
+  });
 }
 
 submitButton.addEventListener('click', async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const toCurrency = toCurrencyDropDown.value;
-    const fromCurrency = fromCurrencyDropDown.value;
+  const toCurrency = toCurrencyDropDown.value;
+  const fromCurrency = fromCurrencyDropDown.value;
 
-    const requestUrl = `${baseApi}?have=${fromCurrency}&want=${toCurrency}&amount=${currencyInput.value}`;
+  const requestUrl = `${baseApi}?have=${fromCurrency}&want=${toCurrency}&amount=${currencyInput.value}`;
 
-    submitButton.disabled = true;
-    try {
-        const request = fetch(requestUrl, { headers: { "X-Api-Key": apiKey }, method: "GET" });
-        outputContent.textContent = "Calculating value..."; // technically we're not but shhhhhh!!!
+  submitButton.disabled = true;
+  try {
+    const request = fetch(requestUrl, { headers: { "X-Api-Key": apiKey }, method: "GET" });
+    outputContent.textContent = "Calculating value..."; // technically we're not but shhhhhh!!!
 
-        const response = await request;
-        const data = await response.json();
+    const response = await request;
+    const data = await response.json();
 
-        outputContent.textContent = `${formatCurrency(fromCurrency, data.old_amount)} is worth ${formatCurrency(toCurrency, data.new_amount)}`;
-    }
-    finally {
-        submitButton.disabled = false;
-    }
+    outputContent.textContent = `${formatCurrency(fromCurrency, data.old_amount)} is worth ${formatCurrency(toCurrency, data.new_amount)}`;
+  }
+  finally {
+    submitButton.disabled = false;
+  }
 });
